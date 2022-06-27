1d847187ade53e8ded0b63184363603be68e32eb
Saiteja Reddy Pottanigari <58398337+tejareddy8888@users.noreply.github.com>
2022-06-28 01:36:31 +0200

    added CNN with autoencoded input and RNN with autoencoded input, label encoded the VIX

--- Readme.md
+++ Readme.md
@@ -18,7 +18,7 @@ drop out layers help in creating subgraph and model averaging kind of concepts
 information coefficient
 bayesian inferance
 prior psoterior
-granger causality
+granger causality (https://www.analyticsvidhya.com/blog/2021/08/granger-causality-in-time-series-explained-using-chicken-and-egg-problem/)
 
 
 Data Ingestion
@@ -44,3 +44,85 @@ Plotting Data in everyphase
 clustering the 
 
 not too much position sizing and all the other volatality into consideration
+
+    x1 | x2| x3| x4| x5| x6| x7| x8
+> |Rho(t)|Inf(t)|SP5(t-5)| SP5(t-4)| SP5(t-3) | SP5(t-3) |SP5(t-1)| SP5(t)|  ---> predict SP5(t+1)
+
+
+## Data Ingestion:
+ -> Load Data 
+
+## Data Preprocessing: 
+-> Normalizing
+-> Label encoding (Seperating Phase variables into phases)
+-> Sliding Window
+-> Autoencoder the Windowed Data
+-> (NC) Further split the data in train 60, val 20, test 20
+
+## Data Forecasting:
+-> Prediction of S&P 500 using model variable using RNN 
+-> using CNN
+
+## Data Evaluation:
+-> (NC) Predicting power test ?? (Calculate error MSE? about accuracy) using the test the model
+-> (NC) Precision testing here like ROC or any other metric
+-> (NC) Precision of the model against individual phase variable's phases
+
+
+
+-> think about other Objective functions to train MSE for accuracy or ROC for precision
+> after discussion, we have decided to go with accuracy, as that is most needed.
+> 
+
+## Trading Strategy
+MSE : 0.93
+
+PredictedValue (-+) (0.07)^2 -->  new Predicted value 
+-> (NC) Trading on Direction (100% in or 100% out) also apply phase variable
+> We have 1 month invest 1 in the 
+> We have 5 timestamps we predict the direction to enter or not, find the return on the invest on every recurring timelag and take the position, if direction is +ve, invest and if direction is -ve , we pose the P&L.
+> We need to fetch the sharpe ratio of the model which evaluates the model strategy.
+> 100% in or 100% out, is not it the plain sharpe ratio of S&P 500.
+
+
+## experiment segLearn
+
+<!-- # Split the data into train test and validation using the windowgenerator as we want the time series methodology while doing this also refer the documents
+# perform pre-classification using the label encoder or any other logic like logistic regression or any classification logic on three signals CPI, Rho, DIL
+# perform Dimensional Reduction
+#     compare the fitting vs overfitting
+# denoising using the autoencoder
+# kalman filter
+# seperating the signal from noise
+
+
+# irrespective of the activation function, applying pooling on the
+
+# drop out layers help in creating subgraph and model averaging kind of concepts
+# information coefficient
+# bayesian inferance
+# prior psoterior
+# granger causality
+
+
+# Data Ingestion
+#     - Data Splitting for validation and
+# Data Preprocessing
+#     - Denoising AutoEncoder
+#     - Kalman Filter
+#     - Dimensional Reduction
+
+# Model:
+#     - Training Strategies
+#     - Cross Validation
+#     - Trading strategies
+#           - Supply shock
+#           - Causality, find the casual dimension
+#           -
+#     - LOSS Functions
+#     - Optimizer choice
+#     - Learning Rate scheduler
+
+# Plotting Data in everyphase
+
+# clustering the -->
--- ourProjectFile.py
+++ ourProjectFile.py
@@ -9,9 +9,11 @@ import tensorflow as tf
 
 def load_data():
     # prepare data
-    data_df = pd.read_excel('./market_data.xlsx',sheet_name='data', engine = 'openpyxl')
-    data_df.set_index('Date',drop=True,inplace=True)
+    data_df = pd.read_excel('./market_data.xlsx',
+                            sheet_name='data', engine='openpyxl')
+    data_df.set_index('Date', drop=True, inplace=True)
     return data_df
+
 
 def normalize_data(df):
     """ This function can also be called as feature scaling like normalization, min-max scaling, 
@@ -22,196 +24,224 @@ def normalize_data(df):
 
 
 class WindowGenerator():
-  def __init__(self, input_width, label_width, shift, input_columns, label_columns, all_columns):
+    def __init__(self, input_width, label_width, shift, input_columns, label_columns, all_columns):
 
-    # Derive the label/target column indices.
-    self.label_columns = label_columns
+        # Derive the label/target column indices.
+        self.label_columns = label_columns
 
-    # Derive feature/signal/input column indices
-    self.input_columns = input_columns
+        # Derive feature/signal/input column indices
+        self.input_columns = input_columns
 
-    self.all_column_indices = {name: i for i, name in enumerate(all_columns)};
+        self.all_column_indices = {
+            name: i for i, name in enumerate(all_columns)}
 
+        # input parameters to configure the window.
+        self.input_width = input_width
+        self.label_width = label_width
+        # offset or shift before the prediction label value.
+        self.shift = shift
+        self.total_window_size = input_width + shift
 
-    # input parameters to configure the window.
-    self.input_width = input_width
-    self.label_width = label_width
-    ## offset or shift before the prediction label value.
-    self.shift = shift
-    self.total_window_size = input_width + shift
+        # the actual length on input size to be taken on every window on the data
+        self.input_slice = slice(0, input_width)
+        # derive the input indices on the data generated after the window application
+        self.input_indices = np.arange(self.total_window_size)[
+            self.input_slice]
 
-    # the actual length on input size to be taken on every window on the data
-    self.input_slice = slice(0, input_width)
-    # derive the input indices on the data generated after the window application
-    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+        # derive the label starting postion
+        self.label_start = self.total_window_size - self.label_width
+        # the actual length on label size to be taken on every window on the data
+        self.labels_slice = slice(self.label_start, None)
+        # derive the label indices on the data generated after the window application
+        self.label_indices = np.arange(self.total_window_size)[
+            self.labels_slice]
 
-    # derive the label starting postion
-    self.label_start = self.total_window_size - self.label_width
-    # the actual length on label size to be taken on every window on the data
-    self.labels_slice = slice(self.label_start, None)
-    # derive the label indices on the data generated after the window application
-    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Input columns: {self.input_columns}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
 
-  def __repr__(self):
-    return '\n'.join([
-        f'Total window size: {self.total_window_size}',
-        f'Input indices: {self.input_indices}',
-        f'Input columns: {self.input_columns}',
-        f'Label indices: {self.label_indices}',
-        f'Label column name(s): {self.label_columns}'])
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.input_columns is not None:
+            inputs = tf.stack([inputs[:, :, self.all_column_indices[name]]
+                              for name in self.input_columns], axis=-1)
+        if self.label_columns is not None:
+            labels = tf.stack([labels[:, :, self.all_column_indices[name]]
+                              for name in self.label_columns], axis=-1)
+        #inputs.set_shape([None, self.input_width, None])
+        #labels.set_shape([None, self.label_width, None])
+        return inputs, labels
 
-  def split_window(self, features):
-      inputs = features[:, self.input_slice, :]
-      labels = features[:, self.labels_slice, :]
-      if self.input_columns is not None:
-        inputs = tf.stack([inputs[:, :, self.all_column_indices[name]] for name in self.input_columns], axis=-1)
-      if self.label_columns is not None:
-        labels = tf.stack([labels[:, :, self.all_column_indices[name]] for name in self.label_columns], axis=-1)
-      #inputs.set_shape([None, self.input_width, None])
-      #labels.set_shape([None, self.label_width, None])
-      return inputs, labels
-
-  def make_dataset(self, data, shuffle = False, batchsize = 500,):
-      data = np.array(data, dtype=np.float32)
-      ds = tf.keras.preprocessing.timeseries_dataset_from_array(data=data, targets=None, sequence_length=self.total_window_size,
-                                                                sequence_stride=1, sampling_rate=1, shuffle=shuffle, batch_size=batchsize)                                                        
-      ds = ds.map(self.split_window)
-      return ds
+    def make_dataset(self, data, shuffle=False, batchsize=500,):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(data=data, targets=None, sequence_length=self.total_window_size,
+                                                                  sequence_stride=1, sampling_rate=1, shuffle=shuffle, batch_size=batchsize)
+        ds = ds.map(self.split_window)
+        return ds
 
 
 class Autoencoder(tf.keras.models.Model):
-  def __init__(self, num_timesteps, num_inputs, num_hidden, kernel_size, pooling):
-    super(Autoencoder, self).__init__()
-    self.num = num_timesteps
-    self.lb = kernel_size
-    self.pooling =pooling
+    def __init__(self, num_timesteps, num_inputs, num_hidden, kernel_size, pooling):
+        super(Autoencoder, self).__init__()
+        self.num = num_timesteps
+        self.lb = kernel_size
+        self.pooling = pooling
 
-    encoder_input = tf.keras.Input(shape=(num_timesteps, num_inputs), name="input")
-    x = tf.keras.layers.Conv1D(filters=num_hidden, kernel_size=kernel_size, activation=None, use_bias=True, padding='causal')(encoder_input)
-    x = tf.keras.layers.MaxPooling1D(self.pooling, strides=self.pooling, padding='same')(x)
-    self.encoder = tf.keras.Model(inputs=encoder_input, outputs=x)
-    decoder_input = tf.keras.Input(shape=(int(num_timesteps/self.pooling), num_hidden))
-    y = tf.keras.layers.Conv1DTranspose(filters=num_inputs, kernel_size=kernel_size, strides=self.pooling, activation=None, use_bias=True, padding='same')(decoder_input)
-    self.decoder = tf.keras.Model(inputs=decoder_input, outputs=y)
+        encoder_input = tf.keras.Input(
+            shape=(num_timesteps, num_inputs), name="input")
+        x = tf.keras.layers.Conv1D(filters=num_hidden, kernel_size=kernel_size,
+                                   activation=None, use_bias=True, padding='causal')(encoder_input)
+        x = tf.keras.layers.MaxPooling1D(
+            self.pooling, strides=self.pooling, padding='same')(x)
+        self.encoder = tf.keras.Model(inputs=encoder_input, outputs=x)
+        decoder_input = tf.keras.Input(
+            shape=(int(num_timesteps/self.pooling), num_hidden))
+        y = tf.keras.layers.Conv1DTranspose(filters=num_inputs, kernel_size=kernel_size,
+                                            strides=self.pooling, activation=None, use_bias=True, padding='same')(decoder_input)
+        self.decoder = tf.keras.Model(inputs=decoder_input, outputs=y)
 
-  def call(self, input):
-    u = self.encoder(input)
-    decoded = self.decoder(u)
-    return decoded
+    def call(self, input):
+        u = self.encoder(input)
+        decoded = self.decoder(u)
+        return decoded
 
 
 if __name__ == '__main__':
+    through_cnn = True
     timesteps = 5
     pooling = 1
 
-    input_columns = ['Rho', 'CPI','_MKT']
+    input_columns = ['Rho', 'CPI', '_MKT']
     target_columns = ['_MKT']
 
-    ## Load the data from the sheet
+    # Load the data from the sheet
     data = load_data()
+    data['VIX_phase'] = data.apply(
+        lambda x: 'low' if x['VOL'] < 12 else 'high' if x['VOL'] > 20 else 'medium', axis=1)
 
-    ## Normalize the entire dataset,
+    print(data.VIX_phase.value_counts())
+    # Normalize the entire dataset,
     data = normalize_data(data)
 
-    ## creating a sliding windowed data
+    # creating a sliding windowed data
     # Few constants like input window of number of time steps width and prediction timesteps width
-    window = WindowGenerator(input_width=timesteps, label_width=1, shift=1, input_columns=input_columns, label_columns=target_columns, all_columns=data.columns)
+    window = WindowGenerator(input_width=timesteps, label_width=1, shift=1,
+                             input_columns=input_columns, label_columns=target_columns, all_columns=data.columns)
 
     # Below is to check the window generation if needed.
     # for batch in windowed_data:
     #   inputs, labels = batch
     #   print("Inputs :",inputs[-1])
     #   print("Lables : ",labels[-1])
-        # sliding window
+    # sliding window
 
-    td = window.make_dataset(data)
-    train_data = td.take(2)
-    val_data = td.skip(2)
+    td = window.make_dataset(data, True, 250)
+    index = 0
+    for i in td:
+        index += 1
+        print(index)
+    train_data = td.take(4)
+    val_data = td.skip(4)
 
     checkpoint_path = "checkpoint/autoencoder.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
 
     # Create a callback that saves the model's weights
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                    save_weights_only=True,
-                                                    verbose=1)
+                                                             save_weights_only=True,
+                                                             verbose=1)
 
     # Training; Hint: play with num_hidden = 1 or 2, and kernel_size
-    AC = Autoencoder(num_timesteps=timesteps, num_inputs=len(input_columns), num_hidden=2, kernel_size=25, pooling=pooling)
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(0.01, decay_steps=50, decay_rate=0.97, staircase=True)
-    AC.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.SGD(learning_rate=lr_schedule), metrics=[tf.metrics.MeanSquaredError(), ])
+    AC = Autoencoder(num_timesteps=timesteps, num_inputs=len(
+        input_columns), num_hidden=2, kernel_size=25, pooling=pooling)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        0.01, decay_steps=50, decay_rate=0.97, staircase=True)
+    AC.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.SGD(
+        learning_rate=lr_schedule), metrics=[tf.metrics.MeanSquaredError(), ])
     AC.run_eagerly = True
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=50, mode='min')
-    AC_history = model.fit(train_data, validation_data=val_data, epochs=500, callbacks=[early_stopping, checkpoint_callback])
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='loss', patience=50, mode='min')
+    AC_history = AC.fit(train_data, validation_data=val_data, epochs=500, callbacks=[
+                        early_stopping, checkpoint_callback])
     AC.summary()
 
-
-    # Loads the weights
-    AC.load_weights(checkpoint_path)
+    # # Loads the weights
+    # AC.load_weights(checkpoint_path)
 
     # Plot loss from Auto Encoder
-    fig, axs = plt.subplots()
-    axs.plot(AC_history.history['loss'])
-    axs.plot(AC_history.history['val_loss'])
-    axs.legend(['training loss', 'validation loss'])
-    
+    fig, axs = plt.subplots(2, 1)
+    axs[0].plot(AC_history.history['loss'])
+    axs[0].plot(AC_history.history['val_loss'])
+    axs[0].legend(['training loss', 'validation loss'])
+
+    # Plot the autoencoded data and actual data
+    # flattened_data = tf.stack(tf.convert_to_tensor(train_data.as_numpy_iterator()))
+
+    # below is to compare the autoencoded_train_data against actual train data
+
+    index = 0
+    x_train = np.concatenate([x for x, y in train_data], axis=0)
+    y_train = np.concatenate([y for x, y in train_data], axis=0)
+
+    autoencoded_train_data = AC.predict(train_data)
+    autoencoded_val_data = AC.predict(val_data)
+    # dumpy_tensor
+
+    # axs[2].plot(dumpy_tensor)
+    # axs[2].plot(AC_history.history['val_loss'])
+    # axs[2].legend(['training loss', 'validation loss'])
+
+    # define sliding window
+    lf = 1      # look forward
+    ks = 3      # kernel size
+    lw = 1      # label width
+    lb = 5
     # Train with RNN
-    RNN = tf.keras.Sequential()
-    RNN.add(tf.keras.layers.SimpleRNN(2, return_sequences=False, return_state=False, activation=None, use_bias=False))
-    RNN.add(tf.keras.layers.Dense(1, activation=None, use_bias=False))
-    RNN.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam(learning_rate=0.01), metrics=[tf.metrics.MeanSquaredError()])
-    RNN.run_eagerly = False
-    RNN_history = RNN.fit(train_data, epochs=500, validation_data=val_data, callbacks=[early_stopping])
-    
-    # Plot loss from RNN
-    fig, axs = plt.subplots()
-    axs.plot(RNN_history.history['loss'])
-    axs.plot(RNN_history.history['val_loss'])
-    axs.legend(['training loss', 'validation loss'])
-    
-    
-    # fig, axs = plt.subplots()
-    # data.loc[:,['DY',]].plot(ax=axs)
-    # plt.show()
+    if through_cnn:
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Conv1D(
+            filters=4, kernel_size=ks, activation='relu', use_bias=False))
+        model.add(tf.keras.layers.MaxPooling1D(
+            pool_size=4, strides=1, padding='same'))
+        model.add(tf.keras.layers.Conv1D(
+            filters=32, kernel_size=ks, activation='relu', use_bias=False))
+        model.add(tf.keras.layers.MaxPooling1D(
+            pool_size=4, strides=1, padding='same'))
+        model.add(tf.keras.layers.Conv1D(filters=4, kernel_size=lb -
+                  2*(ks-1)-(lw-1), activation='relu', use_bias=False))
+        model.add(tf.keras.layers.Dense(units=1))
 
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            0.01, decay_steps=150, decay_rate=0.95, staircase=True)
+        model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.SGD(
+            learning_rate=lr_schedule), metrics=[tf.metrics.MeanSquaredError()])
+        model.run_eagerly = False
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+            monitor='loss', patience=100, mode='min')
+        model_history = model.fit(
+            autoencoded_train_data, validation_data=autoencoded_val_data, epochs=750, batch_size=150, callbacks=[early_stopping])
+        print(model.summary())
 
+    else:
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.SimpleRNN(2, return_sequences=False,
+                                            return_state=False, activation=None, use_bias=False))
+        model.add(tf.keras.layers.Dense(1, activation=None, use_bias=False))
+        model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam(
+            learning_rate=0.01), metrics=[tf.metrics.MeanSquaredError()])
+        model.run_eagerly = False
+        model_history = model.fit(autoencoded_train_data, epochs=500,
+                                  validation_data=autoencoded_val_data, callbacks=[early_stopping])
+        print(model.summary())
 
-## Split the data into train test and validation using the windowgenerator as we want the time series methodology while doing this also refer the documents
-## perform pre-classification using the label encoder or any other logic like logistic regression or any classification logic on three signals CPI, Rho, DIL
-## perform Dimensional Reduction 
-#     compare the fitting vs overfitting
-# denoising using the autoencoder
-# kalman filter
-# seperating the signal from noise
+        # Plot loss from RNN
+    axs[1].plot(model_history.history['loss'])
+    axs[1].plot(model_history.history['val_loss'])
+    axs[1].legend(['training loss', 'validation loss'])
 
-
-# irrespective of the activation function, applying pooling on the 
-
-# drop out layers help in creating subgraph and model averaging kind of concepts
-# information coefficient
-# bayesian inferance
-# prior psoterior
-# granger causality
-
-
-# Data Ingestion
-#     - Data Splitting for validation and
-# Data Preprocessing
-#     - Denoising AutoEncoder
-#     - Kalman Filter
-#     - Dimensional Reduction
-
-# Model: 
-#     - Training Strategies
-#     - Cross Validation
-#     - Trading strategies
-#           - Supply shock
-#           - Causality, find the casual dimension
-#           - 
-#     - LOSS Functions
-#     - Optimizer choice
-#     - Learning Rate scheduler
-
-# Plotting Data in everyphase
-
-# clustering the 
+    plt.show()
